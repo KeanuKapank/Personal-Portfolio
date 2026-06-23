4fd42aa3addd644238044ccd2ec668ca0e406b35
KeanuKapank <keanukapank06@gmail.com>
2026-06-23 21:18:16 +0200

    Chore : Added Flex Display to containers

--- src/components/home/AboutMeSection.tsx
+++ src/components/home/AboutMeSection.tsx
@@ -20,32 +20,38 @@ const AboutMeSection = () => {
               Inside the Brain
             </span>
           </h5>
-          <p className="text-body-sm mb-4">
-            Keanu Kapank, a 22 year old software engineer, I'm a passionate and
-            driven individual with a love for technology and innovation. With a
-            strong background in Information Technology, I have honed my skills
-            in various tools and frameworks, allowing me to create efficient and
-            effective solutions to complex problems. My dedication to continuous
-            learning and improvement has led me to explore new technologies and
-            stay up-to-date with industry trends.
-          </p>
-          <p className="text-body-sm">
-            I'm collaborative by nature and my excellent communication skills
-            make me a valuable team player, always willing to share knowledge
-            and support my colleagues. In my free time, I enjoy contributing to
-            open-source projects, attending tech meetups, and exploring the
-            latest advancements in the tech world.
-          </p>
-          <hr className="line-broken"></hr>
-          <h6>Hobbies: </h6>
-          <ul className="text-sm list-disc list-inside">
-            <li>Jogging</li>
-            <li>Reading</li>
-            <li>Hiking</li>
-            <li>Watching Sport Amabokke</li>
-            <li>Playing Video Games</li>
-          </ul>
-
+          <div className="sm:flex flex-row gap-8">
+            <div>
+              <p className="text-body-sm mb-4">
+                Keanu Kapank, a 22 year old software engineer, I'm a passionate
+                and driven individual with a love for technology and innovation.
+                With a strong background in Information Technology, I have honed
+                my skills in various tools and frameworks, allowing me to create
+                efficient and effective solutions to complex problems. My
+                dedication to continuous learning and improvement has led me to
+                explore new technologies and stay up-to-date with industry
+                trends.
+              </p>
+              <p className="text-body-sm">
+                I'm collaborative by nature and my excellent communication
+                skills make me a valuable team player, always willing to share
+                knowledge and support my colleagues. In my free time, I enjoy
+                contributing to open-source projects, attending tech meetups,
+                and exploring the latest advancements in the tech world.
+              </p>
+            </div>
+            <div>
+              <hr className="line-broken"></hr>
+              <h6>Hobbies: </h6>
+              <ul className="text-sm list-disc list-inside">
+                <li>Jogging</li>
+                <li>Reading</li>
+                <li>Hiking</li>
+                <li>Watching Sport Amabokke</li>
+                <li>Playing Video Games</li>
+              </ul>
+            </div>
+          </div>
           {tags.map((tag) => (
             <span
               key={tag.text}
@@ -53,7 +59,7 @@ const AboutMeSection = () => {
               style={{
                 left: tag.x,
                 top: tag.y,
-                transform: `rotate(${tag.rotate}deg)`
+                transform: `rotate(${tag.rotate}deg)`,
               }}
             >
               {tag.text}
